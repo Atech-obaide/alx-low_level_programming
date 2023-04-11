974864f8ffb6d1f6040aa557f25ed0687c18f78f
Atech-obaide <amosobaide@gmail.com>
2023-04-10 19:11:00 -0700

    updates

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int main (int argc, char *argv[]);
-
-#endif
+int _putchar(char c);                                                           int check_num(char *str);
